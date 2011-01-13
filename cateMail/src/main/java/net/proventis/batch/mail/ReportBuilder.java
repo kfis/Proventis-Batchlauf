a5fe95a9be05486d248bad7851b43ea8ff57caea
Richard Henck <richard.henck@criticalpath.net>
2011-01-13 18:00:35 +0100

    added javaDoc

--- cateMail/src/main/java/net/proventis/batch/mail/ReportBuilder.java
+++ cateMail/src/main/java/net/proventis/batch/mail/ReportBuilder.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package net.proventis.batch.mail;
 
@@ -15,23 +11,39 @@ import net.proventis.criteria.ProjectInformation;
 import net.proventis.criteria.TaskInformation;
 
 /**
- *
+ * Class for constructing a report from given project information.
  * @author gd
  */
 public class ReportBuilder {
 
+	/** project information object */
     private final ProjectInformation p;
 
+    /**
+     * Constructs a reportbuilder.
+     * @param p
+     */
     public ReportBuilder(ProjectInformation p){
         this.p = p;
     }
 
+    /**
+     * Builds the report from header, content and footer.
+     * @return report as string
+     * @throws IOException
+     */
     public String buildReport() throws IOException{
         StringBuilder sb = new StringBuilder();
         sb.append(getHeader()).append(getContent()).append(getfooter());
         return sb.toString();
     }
 
+    /**
+     * Returns a string with the contents of the file "Head.html". Several placeholders in the file are
+     * replaced by the data from the global project information object.
+     * @return string with the contents of the file "Head.html"
+     * @throws IOException
+     */
     private String getHeader() throws IOException{
         StringBuilder sb = new StringBuilder();
         BufferedReader bf = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/Head.html")));
@@ -42,6 +54,12 @@ public class ReportBuilder {
         return sb.toString().replace("{projectname}", p.getProject()).replace("{date}",getReportDate() );
     }
 
+    /**
+     * Returns a string with the contents of the file "ReportSnippet.html". Several placeholders in the file are
+     * replaced by the data from the global project information object.
+     * @return string with the contents of the file "ReportSnippet.html"
+     * @throws IOException
+     */
     private String getContent() throws IOException{
         String snippet;
         StringBuilder sb = new StringBuilder();
@@ -65,6 +83,11 @@ public class ReportBuilder {
         return content.toString();
     }
 
+    /**
+     * Returns a string with the contents of the file "footer.html".
+     * @return string with the contents of the file "footer.html"
+     * @throws IOException
+     */
     private String getfooter() throws IOException{
         StringBuilder sb = new StringBuilder();
         BufferedReader bf = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/Footer.html")));
@@ -75,11 +98,20 @@ public class ReportBuilder {
         return sb.toString();
     }
 
+    /**
+     * Returns the current date as string.
+     * @return String with the current date
+     */
     private String getReportDate() {
         Calendar cal = Calendar.getInstance();
         return cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.MONTH + 1) + "." + cal.get(Calendar.YEAR);
     }
 
+    /**
+     * Formats the given date to have two digits and return it as string.
+     * @param d date to format
+     * @return string of the given date
+     */
     private String formatDate(Date d){
         DecimalFormat df = new DecimalFormat("00");
         return df.format(d.getDate())+"."+df.format(d.getMonth()+1)+"."+(d.getYear()+1900);
